2208162debf1593d364ba0f0c4a88472a01c6798
rachit <whyrachit@gmail.com>
2025-02-27 17:50:14 +0530

    Fixed minor bugs

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Spotify API credentials
-SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", "your_spotify_client_id")
-SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", "your_spotify_client_secret")
-SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI", "http://localhost:8501/callback")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 
 # Additional configuration for Agno integration
-AGNO_API_KEY = os.getenv("AGNO_API_KEY", "your_agno_api_key")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY", "your_groq_api_key")
+AGNO_API_KEY = os.getenv("AGNO_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
